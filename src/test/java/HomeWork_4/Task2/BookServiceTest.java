989ca0232d03c1d599587e15d5de266b4324808a
Olesya <lesusha1@yandex.ru>
2024-01-02 18:53:18 +0300

    homework_4 (task 2) added

--- src/test/java/HomeWork_4/Task2/BookServiceTest.java
+++ src/test/java/HomeWork_4/Task2/BookServiceTest.java
@@ -4,6 +4,9 @@ package HomeWork_4.Task2;
 // из базы данных. Ваша задача написать unit-тесты для BookService, используя Mockito для создания мок-объекта
 // BookRepository.
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,7 +16,7 @@ import static org.mockito.Mockito.*;
 class BookServiceTest {
 
     @Test
-    void testUserServiceWithMockRepository() {
+    void testFindBookById() {
 
         BookRepository bookRepository = mock(BookRepository.class);
         BookService bookService = new BookService(bookRepository);
@@ -26,5 +29,23 @@ class BookServiceTest {
         assertEquals(bookExpected.getAuthor(), book.getAuthor());
         assertEquals(bookExpected.getTitle(), book.getTitle());
         verify(bookRepository, times(1)).findById(id);
+    }
+
+    @Test
+    void testFindAllBooks() {
+
+        BookRepository bookRepository = mock(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+
+        List<Book> expectedBooks = new ArrayList<>();
+        Book book1 = new Book("1", "It", "King");
+        Book book2 = new Book("2", "Shining", "King");
+        expectedBooks.add(book1);
+        expectedBooks.add(book2);
+
+        when(bookRepository.findAll()).thenReturn(expectedBooks);
+        List<Book> books = bookService.findAllBooks();
+        assertEquals(expectedBooks.get(0), books.get(0));
+        verify(bookRepository, times(1)).findAll();
     }
 }
